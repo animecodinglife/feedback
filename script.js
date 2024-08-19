374a6554fe2a47fcbf4b50feb5b96f124e925248
AJAY <lolyak2002@gmail.com>
2024-08-19 18:34:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+const rating=document.querySelectorAll('.rating')
+const ratingsConstainer=document.querySelector('.rating-container')
+const sendBtn = document.querySelector('#send')
+const panel = document.querySelector('#panel')
+let selectedRating = 'Satisfied'
+
+
+ratingsConstainer.addEventListener('click',(e) => {
+    if(e.target.parentNode.classList.contains('rating') && e.target.nextElementSibling) {
+        removeActive()
+        e.target.parentNode.classList.add('active')
+        selectedRating = e.target.nextElementSibling.innerHTML
+    } else if(
+        e.target.parentNode.classList.contains('rating') &&
+        e.target.previousSibling &&
+        e.target.previousElementSibling.nodeName === 'IMG'
+    ) {
+        removeActive()
+        e.target.parentNode.classList.add('active')
+        selectedRating = e.target.innerHTML
+    }
+
+
+})
+
+
+sendBtn.addEventListener('click',(e) => {
+    panel.innerHTML = `
+    <i class="fas fa-heart"></i>
+    <strong>Thank you! </strong>
+    <br>
+    <strong>Feedback: ${selectedRating}</strong>
+    <p>We'll use your feedback to improve our customer support </p>
+    
+    `
+})
+
+
+function removeActive() {
+    for(let i =0; i <rating.length; i++)
+{
+    rating[1].classList.remove('active')
+}
+
+}
